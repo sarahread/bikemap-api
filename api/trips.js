d6df2d24750eb19e9a3e912b220abee520a25d9d
Will Blanchette <wb@wblanchette.com>
2018-02-22 21:11:54 -0700

    Added trips

--- api/trips.js
+++ api/trips.js
@@ -22,7 +22,14 @@ router.post('/', passport.authenticate('jwt', { session: false}), (req, res) => 
   });
 });
 
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get('/', null, (req, res) => {
+  Trip.find({'user._id': ObjectId(req.user._id)}, (err, trips) => {
+    if (err) {
+      return res.status(500).json({ success: false, msg: 'There was a problem retrieving trips: ' + err.message });
+    }
+
+    res.json(trips);
+  });
 });
 
 module.exports = router;
